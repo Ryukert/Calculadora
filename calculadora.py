b52373d735c076d1c2cd25ccc0a71b091de81a2d
Luis Jose <ljmr9807@gmail.com>
2025-04-05 12:01:43 -0600

    erro

--- calculadora.py
+++ calculadora.py
@@ -15,6 +15,8 @@ while True:
     try:
         num1 = float(input("Ingresa el primer número: "))
         num2 = float(input("Ingresa el segundo número: "))
+        match opcion:
+        
     except ValueError:
         print("Entrada inválida. Por favor, ingresa números.")
         continue
